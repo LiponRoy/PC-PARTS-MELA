51a2df6656d79a0b22cf16efd9838bf43bf772e7
Lipon Roy <liponbdbd@gmail.com>
2022-05-21 23:55:23 +0600

    my final project work start from now

---
+++ src/component/carasol/Carasol.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Carasol = () => {
+	return (
+		<div class='my-40 '>
+			<p class='text-2xl'>Welcome to my final project !!</p>
+		</div>
+	);
+};
+
+export default Carasol;
