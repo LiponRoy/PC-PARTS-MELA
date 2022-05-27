de0836e4b14d38b9c46d6543a48442a6e177610d
Lipon Roy <liponbdbd@gmail.com>
2022-05-27 15:30:58 +0600

    Purchase page created with all requirement

--- src/component/Purchase/Purchase.js
+++ src/component/Purchase/Purchase.js
@@ -1,8 +1,13 @@
+import './Purchase.css';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../Firebase.init';
+import { useForm } from 'react-hook-form';
 const Purchase = () => {
+	const [QuantityError, setQuantityError] = useState('');
+	const [btnDisabled, setBtnDisabled] = useState(true);
+
 	const [user] = useAuthState(auth);
 	const { id } = useParams();
 	const navigate = useNavigate();
@@ -15,26 +20,90 @@ const Purchase = () => {
 			.then(data => setSingleProduct(data.SingleProduct));
 	}, []);
 
+	// react hook form
+	const {
+		register,
+		handleSubmit,
+		watch,
+		getValues,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = data => {
+		const avQ = document.getElementById('avQuan').innerHTML;
+		const miniQ = document.getElementById('miniQuan').innerHTML;
+
+		// parseInt(data.minimumOrderQuantity);
+		if (parseInt(data.myQuantity) > parseInt(avQ)) {
+			setQuantityError('');
+			setQuantityError('availableQuantity Over');
+			return;
+		}
+		if (parseInt(data.myQuantity) < parseInt(miniQ)) {
+			setQuantityError('');
+			setQuantityError('les than minimumOrderQuantity');
+			return;
+		}
+		setQuantityError('');
+		console.log(data);
+	};
+
+	// const handleChange = e => {
+	// 	const avQ = document.getElementById('avQuan').innerHTML;
+	// 	const miniQ = document.getElementById('miniQuan').innerHTML;
+
+	// 	// const myValue = getValues('myQuantity');
+
+	// 	if (e.target.value < miniQ) {
+	// 		setBtnDisabled(false);
+	// 	}
+	// 	if (e.target.value > avQ) {
+	// 		setBtnDisabled(false);
+	// 	}
+
+	// 	console.log(e.target.value);
+	// };
+
 	return (
 		<div>
-			<div className='text-center'>
-				<div className='userInfo'>
-					<span>User Name : {user.displayName}</span>
-					<br></br>
-					<span>User Email : {user.email}</span>
-				</div>
-				<div className='imgPart'>
-					<img className='' src={singleProduct.imgUrl} width='220px' alt='' />
-				</div>
+			<div class='hero min-h-screen bg-base-200 myContain'>
+				<div class='hero-content flex-col lg:flex-row'>
+					<div class='hero-content flex-col lg:flex-col'>
+						<img src={singleProduct.imgUrl} class='max-w-sm rounded-lg shadow-2xl' />
+						<h3 className='text-2xl'>Name: {singleProduct.name}</h3>
+						<h4 className='text-3xl font-bold'>$ {singleProduct.price}</h4>
+					</div>
+					<div>
+						<h1 class='text-2xl'>About Product</h1>
+						<p>{singleProduct.description}</p>
 
-				<div className='contentAll'>
-					<div className=''>
-						<h3>Name: {singleProduct.name}</h3>
-						<h4>Desc :{singleProduct.description}</h4>
-						<h4>Price :{singleProduct.price} Tk</h4>
-						<h4>Available Quantity :{singleProduct.availableQuantity}</h4>
-						<h4>Minimum Order Quantity :{singleProduct.minimumOrderQuantity}</h4>
+						<span>User Name:</span>
+						<h1 class='text-2xl'>{user.displayName}</h1>
+						<span>User Email:</span>
+						<h1 class='text-2xl'>{user.email}</h1>
 						<br></br>
+						<span>Available Quantity:</span>
+						<h1 class='text-2xl' id='avQuan'>
+							{singleProduct.availableQuantity}
+						</h1>
+						<span>Minimum Quantity:</span>
+						<h1 class='text-2xl' id='miniQuan'>
+							{singleProduct.minimumOrderQuantity}
+						</h1>
+						<br></br>
+						{/* Form */}
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<span>My Quantity</span>
+							<input className='par-input' placeholder='Your quantity' type='number' {...register('myQuantity', { required: true })} />
+
+							{/* errors will return when field validation fails  */}
+							{errors.minimumOrderQuantity && <span>This field is required</span>}
+
+							<br></br>
+
+							<input class='btn btn-primary' type='submit' value='PLACE ORDER' />
+							<h1 className='text-2xl text-red-500'>{QuantityError}</h1>
+						</form>
 					</div>
 				</div>
 			</div>
