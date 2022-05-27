64408244d6eebefdb017d602c0b615ce7b26ccbc
Lipon Roy <liponbdbd@gmail.com>
2022-05-27 23:51:41 +0600

    verify token

--- src/component/deshboard/myOrder/MyOrder.js
+++ src/component/deshboard/myOrder/MyOrder.js
@@ -1,22 +1,30 @@
 import { useContext, useEffect, useState } from 'react';
-import './MyOrder.css';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../Loading/Loading';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../../Firebase.init';
-
+import { signOut } from 'firebase/auth';
 const MyOrder = () => {
 	const [user] = useAuthState(auth);
 	const [product, setProduct] = useState([]);
 	const navigate = useNavigate();
 
-	// fetch(`http://localhost:4000/getOrder?email= ${user.email}`)
-	//fetch(`http://localhost:4000/order/getAllOrder/${user.email}`)
-
 	useEffect(() => {
 		if (user) {
-			fetch(`http://localhost:4000/getOrder?email=${user.email}`)
-				.then(response => response.json())
+			fetch(`http://localhost:4000/getOrder?email=${user.email}`, {
+				method: 'GET',
+				headers: {
+					authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+				},
+			})
+				.then(response => {
+					if (response.status === 401 || response.status === 403) {
+						signOut(auth);
+						localStorage.removeItem('accessToken');
+						navigate('/');
+					}
+					return response.json();
+				})
 				.then(data => setProduct(data));
 		}
 	}, [user]);
@@ -27,27 +35,23 @@ const MyOrder = () => {
 			.then(response => response.json())
 			.then(data => data);
 	};
-
 	return (
 		<div>
 			<div>
 				{product.length != 0 ? (
 					<div>
 						<div className=' grid md:grid-cols-3'>
-							{product.map(prod => (
-								// display Json Data using map function
-								<div key={prod._id} className='col-md-12'>
-									<div className='Manageproduct '>
-										<div className='allProduct '>
-											<img class='card-img-top' src={prod.imgUrl} alt='Card image cap' />
-											<span className='name'>{prod.name}</span>
-											<h4 className='price'>{prod.price} TK</h4>
-											<h4 className='price'>Email :{prod.email} </h4>
+							{product?.slice(0, 6).map(prod => (
+								<div key={prod._id}>
+									<div class='card '>
+										<img class='card-img-top' src={prod.imgUrl} alt='Card image cap' />
+										<span className='name'>{prod.name}</span>
+										<h4 className='price'>{prod.price} TK</h4>
+										<h4 className='price'>Email :{prod.email} </h4>
 
-											<a onClick={() => deleteProduct(prod._id)} className='btn btn-warning '>
-												CANCLE
-											</a>
-										</div>
+										<a onClick={() => deleteProduct(prod._id)} className='btn btn-warning mngBtn'>
+											Cancle
+										</a>
 									</div>
 								</div>
 							))}
